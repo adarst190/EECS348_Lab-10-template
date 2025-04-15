520fb977e6678e7dd5fc5260d94a78ef86407b12
Adam Darst <adarst79@gmail.com>
2025-04-14 22:13:21 -0500

    Added function to readfile

--- calculator.cpp
+++ calculator.cpp
@@ -1,1 +1,9 @@
 #include "calculator.hpp"
+/*
+Function will parse a double that is a string, and must check if
+the double is valid without using std::stod or std::strtod.
+*/
+
+double parse_number(const std::string &expression){
+    
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
-
+#include <fstream>
 #include "calculator.hpp"
 
-int main(int argc, char *argv[]) {
 
+void readFile(std::string filename){
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "Error opening file." << std::endl;
+        return;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        parse_number(line);
+    }
+
+    file.close();
+}
+
+int main(int argc, char *argv[]) {
+    std::string filename;
+    std::cout << "Enter the name of the file to read: " << std::endl;
+    std::cin >> filename;
+    readFile(filename);
+
+    // Run all unit tests in the file "tests.cpp"
+    //::testing::InitGoogleTest(&argc, argv);
+    //return RUN_ALL_TESTS();
 }
